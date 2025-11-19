c616681327efb06acc450c57936cb02ab07a818c
anttikuosmanen-rgb <antti.kuosmanen@gmail.com>
2025-11-19 19:56:21 +0200

    Update update-prelaunch-data, extend to all prelaunch satellites with Calendar entry. (#28)

--- data/tle/update-prelaunch-data.sh
+++ data/tle/update-prelaunch-data.sh
@@ -1,5 +1,5 @@
 wget https://celestrak.org/NORAD/elements/supplemental/
-cat index.html | grep "sup-gp.php?FILE=starlink-g" | grep -o 'href="sup-gp[^"]*"' | cut -d'"' -f2 | sed 's|^|https://celestrak.org/NORAD/elements/supplemental/|' > dls.txt
+cat index.html | grep "sup-gp.php?FILE=" | grep "Calendar" |  grep -o 'href="sup-gp[^"]*"' | cut -d'"' -f2 | sed 's|^|https://celestrak.org/NORAD/elements/supplemental/|' > dls.txt
 wget -i dls.txt
 cat sup-gp.php* > prelaunch.txt
 rm index.html* dls.txt sup-gp.php*
